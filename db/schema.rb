ab0e435076a8dc659178ccb74c3d0b6f6822b413
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-05 17:07:06 +0430

    Create Recipe and recipe_food tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_121138) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_123333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_121138) do
     t.index ["users_id"], name: "index_foods_on_users_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "foods_id", null: false
+    t.bigint "recipes_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["foods_id"], name: "index_recipe_foods_on_foods_id"
+    t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "prepration_time"
+    t.integer "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_recipes_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -32,4 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_121138) do
   end
 
   add_foreign_key "foods", "users", column: "users_id"
+  add_foreign_key "recipe_foods", "foods", column: "foods_id"
+  add_foreign_key "recipe_foods", "recipes", column: "recipes_id"
+  add_foreign_key "recipes", "users", column: "users_id"
 end
