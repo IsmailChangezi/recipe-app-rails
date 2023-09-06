ae17fa6cc07a54c698574d164f9a4d67af9c283a
Ismail Ali <ismailchangezi801@gmail.com>
2023-09-06 19:59:41 +0500

    Edit View and Fix issues

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
-  belongs_to :users, class_name: "User"
+  belongs_to :users, class_name: 'User'
 
-  validates :name, presence: true
   validates :description, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
