e9b2116215a763bc67c20f159c54c93979951969
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-06 21:24:14 +0430

    Add test for recipe-food models and updae recipe-food's model

---
+++ spec/models/recipe_food_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  user1 = User.new(name: 'ismail', email: "ismai4l12@gamil.com", password: "123456") 
+  food = Food.new(name: 'potato', measurement_unit:'1gr', price: 12, users: user1)
+  recipe = Recipe.new(name: 'potato',  preparation_time: "20 munites", cooking_time: "10 munites",description: 'Potato is a popular mail in the world', users: user1)
+  recipe_food = RecipeFood.new(quantity: 13, foods: food, recipes: recipe) 
+    it 'Food object must be valid' do
+        expect(recipe_food).to be_valid
+    end
+
+    it 'Food has teh name' do
+        expect(recipe_food.quantity).to eq(13)
+    end
+end
