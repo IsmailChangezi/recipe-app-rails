17212abcdad9fb1ec21e1d28f30503a2892ce373
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-06 20:56:39 +0430

    Add test for food model and update user test model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   subject { User.new(name: 'AliShah',email: "ali1243@gmail.com") }
+  user = User.new(name: 'ismail', email: "ismai4l12@gamil.com", password: "123456") 
 
   it 'name should not be present' do
     subject.name = nil
     expect(subject).to_not be_valid
+  end
+
+  it "subject two " do
+    expect( user ).to be_valid
   end
 
   it 'name shoud be AliShah' do
