d67a9e3600cbc88b6246620b94731e71c17f5d4b
Ismail Ali <ismailchangezi801@gmail.com>
2023-09-07 20:07:30 +0500

    Public Recipes working and bug fixes

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :users, class_name: 'User'
-
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
+  
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
