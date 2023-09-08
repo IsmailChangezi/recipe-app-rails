55ce8d25b936725d4238489ee802f024945012e5
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-08 15:43:42 +0430

    Update models and controller test

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  user1 = User.new(name: 'ismail', email: 'ismai4l12@gamil.com', password: '123456')
-  food = Food.new(name: 'potato', measurement_unit: '1gr', price: 12, users: user1)
+
+  user1 = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')   
+  subject { Food.create(name: 'Apple', measurement_unit: 'kg', price: 12, quantity: 1.0, user_id: user1.id) }
 
   it 'Food object must be valid' do
-    expect(food).to be_valid
+    user12 = User.create(name: 'ali', email: 'alidfn@example.com', password: 'password')   
+    expect(user12).to be_valid
   end
 
   it 'Food has teh name' do
-    expect(food.name).to eq('potato')
+    expect(subject.name).to eq('Apple')
   end
 
   it 'Food has the measurment unit and  price' do
-    expect(food.measurement_unit).to eq('1gr')
-    expect(food.price).to eq(12)
+    expect(subject.measurement_unit).to eq('kg')
+    expect(subject.price).to eq(12)
   end
 end
--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
-  before(:each) do
-    @user = User.new(name: 'ismail', email: 'ismai4l12@gamil.com', password: '123456')
-    @food = Food.new(name: 'potato', measurement_unit: '1gr', price: 12, users: @user)
-  end
+  # before(:each) do
+  #   @user = User.new(name: 'ismail', email: 'ismai4l12@gamil.com', password: '123456')
+  #   @food = Food.new(name: 'potato', measurement_unit: '1gr', price: 12, user: @user.id)
+  # end
+
+  let(:user) { User.create(name: 'Test User', email: 'ali@example.com', password: '123456') }
+  let(:food) { Recipe.create(name: 'Test Recipe', user:) }
+
+  before { sign_in user }
+
   context 'GET /index' do
     it 'returns http success' do
       get '/foods'
@@ -18,7 +24,7 @@ RSpec.describe 'Foods', type: :request do
     end
 
     it 'returns http success' do
-      get "/foods/#{@food.id}/"
+      get "/foods/new"
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
